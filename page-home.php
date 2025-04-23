6b67c4d6825105a33fd7922c4c48afd096e472ca
fxlks <karmar01@easv365.dk>
2025-04-23 10:54:21 +0200

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
 
 <main class="homepage">
-    <!-- Left content section for everything except the video -->
     <div class="left-content">
         <div class="hero-text">
             <h1 class="welcome"><?php the_field('welcome'); ?></h1>
             <a href="<?php echo site_url('/portfolio'); ?>" class="btn_view">View My Work</a>
         </div>
 
-        <!-- About Preview Section -->
         <section class="about-preview">
             <div class="about-container">
                 <div class="about-text">
@@ -21,8 +19,6 @@
                 </div>
             </div>
         </section>
-
-        <!-- Contact Section -->
         <div class="homepage-content">
             <div class="left-content">
                 <div class="hero-text">
@@ -33,16 +29,15 @@
         </div>
     </div>
 
-    <!-- Video container will be placed to the right -->
-    <?php 
-    $video_url = get_field('video'); 
-    if ($video_url): ?>
-        <div class="video-container">
-            <video controls>
-                <source src="<?php echo esc_url($video_url); ?>" type="video/mp4">
-            </video>
-        </div>
-    <?php endif; ?>
+    <div class="video-container">
+        <iframe width="100%" height="315" 
+            src="https://www.youtube.com/embed/oCQ7CX4cZ_M" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+    </div>
 </main>
 
 <?php get_footer(); ?>
